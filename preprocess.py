8a45d7728f8213672c8a92a1083b41e172158c5b
Ethan Hargrove <ethanhargrove99@gmail.com>
2023-04-24 18:28:22 +0100

    "Get car steering"

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
+import numpy as np
 import numba as nb
 import cv2
-import numpy as np
 
 @nb.njit(fastmath=True)
 def contrast_img(img, colours):
@@ -16,7 +16,7 @@ def contrast_img(img, colours):
                 dist = 0
                 for x, y in zip(rgb_pixel, colour):
                     dist += np.abs(x-y) ** 2
-                dists[k] = dist ** (1/2)
+                dists[k] = dist ** (1/2) 
             min_val = dists[0]
             min_ind = 0
             for l in range(1, len(colours)):
@@ -24,6 +24,7 @@ def contrast_img(img, colours):
                     min_val = dists[l]
                     min_ind = l
             rgb_img[i,j] = colours[min_ind]
+            
     return rgb_img
 
 
@@ -38,6 +39,7 @@ def rgb_to_grey(img):
         for j, rgb_pixel in enumerate(row):
             # Compute weighted sum of RGB channels
             grey_img[i, j] = 0.2989 * rgb_pixel[0] + 0.5870 * rgb_pixel[1] + 0.1140 * rgb_pixel[2]
+
     return grey_img
 
 
@@ -46,7 +48,7 @@ def process_img(img, crop="box", pool_size=2, contrast=True, greyscale=True, nor
     Pre-process the image
     """
     if crop == "box":
-        # crop unnecessary pixels
+        # Crop unnecessary pixels
         img = img[12:-12, 12:-12]
         
     if pool_size is not None:
@@ -68,3 +70,36 @@ def process_img(img, crop="box", pool_size=2, contrast=True, greyscale=True, nor
         img = img.astype(np.float32) / 255
         
     return img
+
+@nb.njit(fastmath=True)
+def get_speed(img):
+    """
+    Extract the car's true speed from the image.
+    Range -> [0,1]
+    """
+    speed_bar = rgb_to_grey(img[84:94,13:14])/255
+    num_ones = 0
+    for pixel in speed_bar:
+        if pixel >= 0.5:
+            num_ones += 1
+
+    return num_ones / len(speed_bar)
+
+
+@nb.njit(fastmath=True)
+def get_steer(img):
+    """
+    Extract the steering wheel position from the image.
+    Range -> [-1,1]
+    """
+    left_bar   = img[89:90, 38:48, 1:2][0] / 253
+    right_bar  = img[89:90, 48:58, 1:2][0] / 253
+    left_ones  = 0
+    right_ones = 0
+    for left_pixel, right_pixel in zip(left_bar, right_bar):
+        if left_pixel >= 0.5:
+            left_ones += 1
+        if right_pixel >= 0.5:
+            right_ones += 1
+
+    return (right_ones / len(right_bar)) - (left_ones / len(left_bar))
