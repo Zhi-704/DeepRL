6d191a6da17ee707eef884c5746e6ecfd13c6a88
Ethan Hargrove <ethanhargrove99@gmail.com>
2023-04-30 18:08:22 +0100

    clip

--- tools.py
+++ tools.py
@@ -1,5 +1,7 @@
 import numpy as np
+import numba as nb
 
+@nb.njit(fastmath=True)
 def GAE(deltas, gamma, lam):
     length = len(deltas)
     output = np.zeros(length)
@@ -9,3 +11,20 @@ def GAE(deltas, gamma, lam):
         else:
             output[i] = deltas[i] + gamma*lam * output[i+1]
     return output
+
+
+@nb.njit(fastmath=True)
+def standardize(values):
+    """Standardize by subtracting the mean and dividing by the standard deviation."""
+    mean = np.mean(values)
+    std = np.std(values)
+    return (values - mean) / std
+
+def clip(value, ϵ):
+    """Clip the value within [1 - ϵ, 1 + ϵ]"""
+    if value < (1 - ϵ):
+        return (1 - ϵ)
+    if value > (1 + ϵ):
+        return (1 + ϵ)
+    else:
+        return value
